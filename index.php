6a5763170763665767b4c587e558397ed15e0ee8
rhiaro <amy@rhiaro.co.uk>
2016-10-27 12:06:57 +0900

    Use template for headers

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
+<? $index = true; ?>
 <? include 'top.php'; ?>
 
         <main>
             <article about="" typeof="schema:Article">
-                <header>
-                    <object data="media/images/linkedresearch_white.png" rel="schema:image" resource="media/images/linkedresearch.svg" type="image/svg+xml"><span property="schema:name" content="Linked Research logo"></span></object>
-                    <hgroup>
-                        <h1 property="schema:name">Linked Research</h1>
-                        <h2>Enabling accessible knowledge</h2>
-                    </hgroup>
-                </header>
+                <? include 'header_large.php'; ?>
                 
                 <div datatype="rdf:HTML" id="content" property="schema:description">
                     <ul id="parts">
--- venues.php
+++ venues.php
@@ -3,12 +3,7 @@
 
         <main>
             <article about="" typeof="schema:Article">
-                <header>
-                    <object data="media/images/linkedresearch_white.png" rel="schema:image" resource="media/images/linkedresearch.svg" type="image/svg+xml" width="64px"><span property="schema:name" content="Linked Research logo"></span></object>
-                    <hgroup>
-                        <h1 property="schema:name">Linked Research</h1>
-                    </hgroup>
-                </header>
+                <? include 'header_small.php'; ?>
                 
                 <div datatype="rdf:HTML" id="content" property="schema:description">
                     
