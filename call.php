c5fcd1085a654aec5d252387e7e3dedc7ef8937e
rhiaro <amy@rhiaro.co.uk>
2016-10-27 20:28:46 +0900

    Call from old homepage, small mods

--- call.php
+++ call.php
@@ -9,7 +9,54 @@
                     <section id="call-for-lr" rel="schema:hasPart" resource="#call-for-lr">
                         <h2 property="schema:name">Call for Linked Research</h2>
                         <div datatype="rdf:HTML" property="schema:description">
-                            <p>...</p>
+                            <section id="motivation" rel="schema:hasPart" resource="#motivation">
+                                <h3 property="schema:name">Motivation</h3>
+                                <div datatype="rdf:HTML" property="schema:description">
+                                    <p id="no-central-authority" about="#no-central-authority" typeof="deo:Motivation" property="schema:description doap:shortdesc">There is no central authority to judge the value of your contributions. You do not need permission to publish! Control your own research and communication.</p>
+                                </div>
+                            </section>
+
+                            <section id="what-is-this" rel="schema:hasPart" resource="#what-is-this">
+                                <h3 property="schema:name">What is this?</h3>
+                                <div datatype="rdf:HTML" property="schema:description">
+                                    <p>Linked Research is set out to socially and technically enable researchers to take full control, ownership, and responsibility of their knowledge, and have their contributions accessible to society at maximum capacity. Linked Research calls for dismantling the archaic and artificial barriers that get in the way of this.</p>
+
+                                    <p>There is no central authority to judge the value of your contributions. You do not need permission to publish! Control your own research and communication. The Web already enables you to be the publisher!</p>
+
+                                    <p>This is not something hypothetical or a dream for the future, it is completely possible today and there are ongoing initiatives and tooling to make it so.</p>
+
+                                    <p>Linked Research’s core goals:</p>
+
+                                    <ul>
+                                        <li>Publishing and ownership of documents and data</li>
+                                        <li>Discovery and reuse of knowledge and data</li>
+                                        <li>User experience and tooling</li>
+                                    </ul>
+
+                                    <p>If you are an academic researcher, reconsider and question the tooling or rules that you are asked to follow e.g:</p>
+
+                                    <ul>
+                                        <li>Academic paywalls</li>
+                                        <li>Information silos such as third-party publishers</li>
+                                        <li>Strictly desktop or “paper user interfaces” e.g., (La)TeX, PDF/Word</li>
+                                        <li>Blind reviews</li>
+                                    </ul>
+
+                                    <p>We can work towards solutions where researchers can publish and consume research objects, as well as reviews and annotations that are open and both human and machine-friendly.</p>
+                                </div>
+                            </section>
+
+                            <section id="what-is-this" rel="schema:hasPart" resource="#what-is-this">
+                                <h3 property="schema:name">How to Linked Research</h3>
+                                <div datatype="rdf:HTML" property="schema:description">
+                                    <ul>
+                                        <li id="access"><i class="fa fa-universal-access"></i> <em>Access</em>: Publish your work under a webspace that you control and trust e.g., your personal website, at your institution's webspace given to you, or library, or even at your company.</li>
+                                        <li id="linked-open-data"><i class="fa fa-cloud"></i> <em>Linked Open Data</em>: Make sure your articles and annotations are human and machine-readable, interlinked with other information out there.</li>
+                                        <li id="user-experience"><i class="fa fa-user"></i> <em>User Experience</em>: Take your articles further and enrich with user interactions to better communicate your ideas. Think beyond the boring fixed, non-interactive views.</li>
+                                        <li id="open-communication"><i class="fa fa-bullhorn"></i> <em>Open communication</em>: Announce your ideas and invite your community to give feedback. Think beyond the minimal communication with organising or program committees. Encourage and help others to do the same.</li>
+                                    </ul>
+                                </div>
+                            </section>
                         </div>
                     </section>
 
