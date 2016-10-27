8ff5796970ed5bb5d451f95fb723a5702963ee46
rhiaro <amy@rhiaro.co.uk>
2016-10-27 11:54:32 +0900

    Pretty URLs

--- top.php
+++ top.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <title><?=isset($title) ? $title : "Linked Research"?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="media/css/makeitso.css" media="all" rel="stylesheet" title="MakeItSo" />
+        <link href="/media/css/makeitso.css" media="all" rel="stylesheet" title="MakeItSo" />
         <link href="https://dokie.li/media/css/lncs.css" media="all" rel="stylesheet alternate" title="LNCS" />
         <link href="https://dokie.li/media/css/acm.css" media="all" rel="stylesheet alternate" title="ACM" />
         <link href="https://dokie.li/media/css/do.css" rel="stylesheet" media="all" />
@@ -20,10 +20,10 @@
     <body about="" typeof="schema:CreativeWork sioc:Post prov:Entity" prefix="rdf: http://www.w3.org/1999/02/22-rdf-syntax-ns# rdfs: http://www.w3.org/2000/01/rdf-schema# owl: http://www.w3.org/2002/07/owl# xsd: http://www.w3.org/2001/XMLSchema# dcterms: http://purl.org/dc/terms/ foaf: http://xmlns.com/foaf/0.1/ v: http://www.w3.org/2006/vcard/ns# pimspace: http://www.w3.org/ns/pim/space# cc: http://creativecommons.org/ns# skos: http://www.w3.org/2004/02/skos/core# prov: http://www.w3.org/ns/prov# schema: http://schema.org/ rsa: http://www.w3.org/ns/auth/rsa# cert: http://www.w3.org/ns/auth/cert# cal: http://www.w3.org/2002/12/cal/ical# wgs: http://www.w3.org/2003/01/geo/wgs84_pos# org: http://www.w3.org/ns/org# biblio: http://purl.org/net/biblio# bibo: http://purl.org/ontology/bibo/ book: http://purl.org/NET/book/vocab# ov: http://open.vocab.org/terms/ doap: http://usefulinc.com/ns/doap# dbr: http://dbpedia.org/resource/ dbp: http://dbpedia.org/property/ sio: http://semanticscience.org/resource/ opmw: http://www.opmw.org/ontology/ deo: http://purl.org/spar/deo/ doco: http://purl.org/spar/doco/ cito: http://purl.org/spar/cito/ fabio: http://purl.org/spar/fabio/ oa: http://www.w3.org/ns/oa# solid: http://www.w3.org/ns/solid/terms#">
         <nav>
             <ul>
-                <li><a href="articles.html"><i class="fa fa-align-left"> Articles</i></a></li>
-                <li><a href="faq.html"><i class="fa fa-question-circle"> FAQ</i></a></li>
-                <li><a href="resources.html"><i class="fa fa-puzzle-piece"> Resources</i></a></li>
-                <li><a href="venues.html"><i class="fa fa-certificate"> Venues</i></a></li>
+                <li><a href="/articles/"><i class="fa fa-align-left"> Articles</i></a></li>
+                <li><a href="/faq/"><i class="fa fa-question-circle"> FAQ</i></a></li>
+                <li><a href="/resources/"><i class="fa fa-puzzle-piece"> Resources</i></a></li>
+                <li><a href="/venues/"><i class="fa fa-certificate"> Venues</i></a></li>
                 <li><a href="https://github.com/linkedresearch"><i class="fa fa-github"> Github</i></a></li>
             </ul>
         </nav>
