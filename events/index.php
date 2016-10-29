b66e6c274564d0794aa104b7787292c9998cd65d
Sarven Capadisli <info@csarven.ca>
2016-10-29 12:03:05 +0200

    Use calls and events/

---
+++ events/index.php
@@ -0,0 +1,34 @@
+<? $title = "Linked Research meetups and events"; ?>
+<? include '../top.php'; ?>
+        <main>
+            <article about="" typeof="schema:Article">
+                <? include '../header-small.php'; ?>
+                <div datatype="rdf:HTML" id="content" property="schema:description">
+                    <section id="meetups-and-events" rel="schema:hasPart" resource="#meetups-and-events">
+                        <h2 property="schema:name">Meetups and Events</h2>
+                        <div datatype="rdf:HTML" property="schema:description">
+                            <p>Whilst our <a href="/calls">call for contributions</a> is ongoing and online, sometimes we like to get together to talk about things in person. This may be regarding publishing our 'normal' research work according to Linked Research principles, or more specifically about the work we are doing to advance the understanding, tooling and culture that goes towards enabling Linked Research as a possibility for everyone.</p>
+
+                            <p>If you know of or want plan a meetup or event of any size to talk about Linked Research or a related topic, let us know and we'll list it here.</p>
+                        </div>
+                    </section>
+
+                    <section id="events-in-planning" rel="schema:hasPart" resource="#events-in-planning">
+                        <h2 property="schema:name">Events in planning</h2>
+                        <div datatype="rdf:HTML" property="schema:description">
+                            <ul>
+                                <li id="www2017">
+                                    <h3>WWW 2017 Workshop (early April 2017)</h3>
+
+                                    <p>Three Linked Research advocates (<a href="http://csarven.ca/#i">Sarven Capadisli</a>, <a href="https://rhiaro.co.uk/#me">Amy Guy</a> and <a href="http://www.oerc.ox.ac.uk/people/David%20De%20Roure">Dave de Roure</a>) have submitted a <a href="/events/www2017/workshop-proposal">workshop proposal</a> to <a href="http://www2017.com.au/">The World Wide Web Conference 2017</a> entitled <em>Enabling Decentralised Scholarly Communication</em>. We invite all those who are interested to volunteer for our program committee.</p>
+
+                                    <p>We also invite those who are thinking about submitting to our <a href="/calls#call-for-enabling-linked-research">Call for Enabling Linked Research</a> to submit by the workshop deadline if you would like to present your work in person at WWW2017.</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </section>
+
+                </div>
+            </article>
+        </main>
+<? include '../end.php'; ?>
--- top.php
+++ top.php
@@ -22,10 +22,10 @@
                 <ul>
                     <li><a href="/"><i class="fa fa-home"> Home</i></a></li>
                     <li><a href="/articles"><i class="fa fa-align-left"> Articles</i></a></li>
-                    <li><a href="/call"><i class="fa fa-bullhorn"> Call</i></a></li>
+                    <li><a href="/calls"><i class="fa fa-bullhorn"> Calls</i></a></li>
                     <li><a href="/resources"><i class="fa fa-puzzle-piece"> Resources</i></a></li>
                     <li><a href="/venues"><i class="fa fa-certificate"> Venues &amp; Publishers</i></a></li>
-                    <li><a href="/events"><i class="fa fa-globe"> Meetups &amp; Events</i></a></li>
+                    <li><a href="/events/"><i class="fa fa-globe"> Meetups &amp; Events</i></a></li>
                     <li><a href="/faq"><i class="fa fa-question-circle"> FAQ</i></a></li>
                     <li><a href="https://github.com/linkedresearch"><i class="fa fa-github"> Github</i></a></li>
                 </ul>
