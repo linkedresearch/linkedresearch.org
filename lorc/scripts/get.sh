b5ef3b7f9034270d6194f688b2cb5d02aaa89fda
Sarven Capadisli <info@csarven.ca>
2017-02-08 12:20:19 +0100

    Towards Linked Open Research Cloud

---
+++ lorc/scripts/get.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+root=$(dirname `pwd`)
+pathToData="$root"/data;
+mkdir "$pathToData";
+
+while read uri; do
+  if [[ ! "$uri" =~ ^# ]]; then
+    urlencoded=$(echo "$uri" | perl -lpe 's/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg')
+    curl "http://rdf.greggkellogg.net/distiller?format=ntriples&uri=""$uri" | sort -u > "$pathToData"/"$urlencoded".nt
+#    sort -u "$dataPath""$urlencoded".nt > "$dataPath""$urlencoded".nt
+
+#    rapper -g "$uri" | sort -u > "$dataPath""$urlencoded".nt
+    echo -e "$urlencoded\n";
+  fi
+  done < uri-list
