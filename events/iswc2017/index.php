1006742dbbf5aab387fecfd28259a6bef2360142
rhiaro <amy@rhiaro.co.uk>
2017-03-16 10:17:00 +0800

    Proposal for ISWC2017

---
+++ events/iswc2017/index.php
@@ -0,0 +1,23 @@
+<? $title = "Linked Research at ISWC 2017"; ?>
+<? include '../../top.php'; ?>
+        <main>
+            <article about="" typeof="schema:Article">
+                <? include '../../header-small.php'; ?>
+
+                <dl id="document-inbox">
+                    <dt>Notifications Inbox</dt>
+                    <dd><a href="https://linkedresearch.org/inbox/linkedresearch.org/events/eswc2017/" rel="ldp:inbox">inbox/</a></dd>
+                </dl>
+
+                <div datatype="rdf:HTML" id="content" property="schema:description">
+                    <section id="enabling-decentralised-scholarly-communication" rel="schema:hasPart" resource="#enabling-decentralised-scholarly-communication">
+                        <h2 property="schema:name">Enabling Decentralised Scholarly Communication</h2>
+                        <div datatype="rdf:HTML" property="schema:description">
+                            <p>We have submitted a <a href="workshop-proposal">workshop proposal</a> to <a href="http://iswc2017.semanticweb.org/">ISWC2017</a>.</p>
+
+                        </div>
+                    </section>
+                </div>
+            </article>
+        </main>
+<? include '../../end.php'; ?>
