ef723395fe38ab2638a76afde2f48536febf3aec
Sarven Capadisli <info@csarven.ca>
2017-05-30 10:48:47 +0200

    Add reviews

--- events/eswc2017/index.php
+++ events/eswc2017/index.php
@@ -31,19 +31,46 @@
 
                                     <ul>
                                         <li>09:00 — 09:15: Hello world!</li>
-                                        <li>09:15 — 09:30: <cite><a href="http://www.oerc.ox.ac.uk/sites/default/files/users/user384/scholarly-social-machines.html">Scholarly Social Machines</a></cite> [<a href="http://web.archive.org/web/20170528223929/http://www.oerc.ox.ac.uk/sites/default/files/users/user384/scholarly-social-machines.html">archived</a>], <a href="http://www.oerc.ox.ac.uk/people/dder">David De Roure</a></li>
+                                        <li>09:15 — 09:30: <cite><a href="http://www.oerc.ox.ac.uk/sites/default/files/users/user384/scholarly-social-machines.html">Scholarly Social Machines</a></cite> [<a href="http://web.archive.org/web/20170528223929/http://www.oerc.ox.ac.uk/sites/default/files/users/user384/scholarly-social-machines.html">archived</a>], <a href="http://www.oerc.ox.ac.uk/people/dder">David De Roure</a>
+                                            <dl>
+                                                <dt>Reviews</dt>
+                                                <dd><a href="https://rhiaro.co.uk/2017/05/response-to-scholarly-social-machines">https://rhiaro.co.uk/2017/05/response-to-scholarly-social-machines</a> [<a href="http://web.archive.org/web/20170530082055/https://rhiaro.co.uk/2017/05/response-to-scholarly-social-machines">archived</a>], <a href="https://rhiaro.co.uk/#me">Amy Guy</a></dd>
+                                            </dl>
+                                        </li>
 
                                         <li>09:30 — 10:00: Discussion: <cite><a href="https://gist.github.com/RubenVerborgh/caa5d9784b584b0f6aea0a1469f73b70">Best practices for self-publishing scholarly metadata</a></cite> [<a href="http://web.archive.org/web/20170528223224/https://gist.github.com/RubenVerborgh/caa5d9784b584b0f6aea0a1469f73b70">archived</a>], led by <a href="https://ruben.verborgh.org/">Ruben Verborgh</a></li>
                                         <li>10:00 — 10:30: Discussion: Decentralisation - what did third-party publishers ever do for us?</li>
 
                                         <li>10:30 — 11:00: Take a break</li>
 
-                                        <li>11:00 — 11:15: <cite><a href="https://openscience.adaptcentre.ie/publications/2017/eswc/">Utilising Semantic Web Ontologies to Publish Experimental Workflows</a></cite> [<a href="http://web.archive.org/web/20170528223523/https://openscience.adaptcentre.ie/publications/2017/eswc/">archived</a>], <a href="https://research.harshp.com/">Harshvardhan J Pandit</a>, Ensar Hadziselimovic, Dave Lewis</li>
-                                        <li>11:15 — 11:30: <cite><a href="https://marco-brambilla.com/2017/05/11/instrumenting-continuous-knowledge-extraction-sharing-and-benchmarking/">Instrumenting Continuous Knowledge Extraction, Sharing, and Benchmarking</a></cite> [<a href="http://web.archive.org/web/20170528223747/https://marco-brambilla.com/2017/05/11/instrumenting-continuous-knowledge-extraction-sharing-and-benchmarking/">archived</a>], <a href="https://marco-brambilla.com/">Marco Brambilla</a>, Emanuele Della Valle, Andrea Mauri, Riccardo Tommasini</li>
+                                        <li>11:00 — 11:15: <cite><a href="https://openscience.adaptcentre.ie/publications/2017/eswc/">Utilising Semantic Web Ontologies to Publish Experimental Workflows</a></cite> [<a href="http://web.archive.org/web/20170528223523/https://openscience.adaptcentre.ie/publications/2017/eswc/">archived</a>], <a href="https://research.harshp.com/">Harshvardhan J Pandit</a>, Ensar Hadziselimovic, Dave Lewis
 
-                                        <li>11:30 — 11:45: <cite><a href="https://www.fiduswriter.org/2017/05/24/decentralized-creation-of-academic-documents-using-a-network-attached-storage-nas-server/">Decentralized creation of academic documents using a Network Attached Storage (NAS) server</a></cite> [<a href="http://web.archive.org/web/20170528223843/https://www.fiduswriter.org/2017/05/24/decentralized-creation-of-academic-documents-using-a-network-attached-storage-nas-server/">archived</a>, <a href="https://www.johanneswilm.org/">Johannes Wilm</a>, Afshin Sadeghi, Christoph Lange, Philipp Mayr</li>
+                                            <dl>
+                                                <dt>Reviews</dt>
+                                                <dd><a href="https://rhiaro.co.uk/2017/05/review-of-utilising-semantic-workflows">https://rhiaro.co.uk/2017/05/review-of-utilising-semantic-workflows</a> [<a href="http://web.archive.org/web/20170530082638/https://rhiaro.co.uk/2017/05/review-of-utilising-semantic-workflows">archived</a>, <a href="https://rhiaro.co.uk/#me">Amy Guy</a></dd>
+                                                <dd><a href="http://homepages.inf.ed.ac.uk/ppapapan/?p=189">http://homepages.inf.ed.ac.uk/ppapapan/?p=189</a> [<a href="http://web.archive.org/web/20170530081845/http://homepages.inf.ed.ac.uk/ppapapan/?p=189">archived</a>], <a href="http://homepages.inf.ed.ac.uk/ppapapan/">Petros Papapanagiotou</a></dd>
+                                                <dd><a href="https://raw.githubusercontent.com/dgarijo/test/gh-pages/onlineReview/Review%20Utilising%20Semantic%20Web%20Ontologies%20to%20publish%20Experimental%20Workflows.txt">https://raw.githubusercontent.com/dgarijo/test/gh-pages/onlineReview/Review%20Utilising%20Semantic%20Web%20Ontologies%20to%20publish%20Experimental%20Workflows.txt</a> [<a href="http://web.archive.org/web/20170530083451/https://raw.githubusercontent.com/dgarijo/test/gh-pages/onlineReview/Review%20Utilising%20Semantic%20Web%20Ontologies%20to%20publish%20Experimental%20Workflows.txt">archived</a>], <a href="http://dgarijo.com/">Daniel Garijo</a></dd>
+                                            </dl>
+                                        </li>
 
-                                        <li>11:45 — 12:30: Open demos, position statements, lightning talks (5mins, <a href="https://public.etherpad-mozilla.org/p/edsc2017">sign up here</a>) and other topics</li>
+                                        <li>11:15 — 11:30: <cite><a href="https://marco-brambilla.com/2017/05/11/instrumenting-continuous-knowledge-extraction-sharing-and-benchmarking/">Instrumenting Continuous Knowledge Extraction, Sharing, and Benchmarking</a></cite> [<a href="http://web.archive.org/web/20170528223747/https://marco-brambilla.com/2017/05/11/instrumenting-continuous-knowledge-extraction-sharing-and-benchmarking/">archived</a>], <a href="https://marco-brambilla.com/">Marco Brambilla</a>, Emanuele Della Valle, Andrea Mauri, Riccardo Tommasini
+                                            <dl>
+                                                <dt>Reviews</dt>
+                                                <dd><a href="https://rhiaro.co.uk/2017/05/review-of-instrumenting">https://rhiaro.co.uk/2017/05/review-of-instrumenting</a> [<a href="http://web.archive.org/web/20170530084916/https://rhiaro.co.uk/2017/05/review-of-instrumenting">archived</a>], <a href="https://rhiaro.co.uk/#me">Amy Guy</a></dd>
+                                            </dl>
+                                        </li>
+
+                                        <li>11:30 — 11:45: <cite><a href="https://www.fiduswriter.org/2017/05/24/decentralized-creation-of-academic-documents-using-a-network-attached-storage-nas-server/">Decentralized creation of academic documents using a Network Attached Storage (NAS) server</a></cite> [<a href="http://web.archive.org/web/20170528223843/https://www.fiduswriter.org/2017/05/24/decentralized-creation-of-academic-documents-using-a-network-attached-storage-nas-server/">archived</a>], <a href="https://www.johanneswilm.org/">Johannes Wilm</a>, Afshin Sadeghi, Christoph Lange, Philipp Mayr
+
+                                            <dl>
+                                                <dt>Reviews</dt>
+                                                <dd><a href="https://rhiaro.co.uk/2017/05/review-of-creation-nas">https://rhiaro.co.uk/2017/05/review-of-creation-nas</a> [<a href="http://web.archive.org/web/20170530084146/https://rhiaro.co.uk/2017/05/review-of-creation-nas">archived</a>], <a href="https://rhiaro.co.uk/#me">Amy Guy</a></dd>
+
+                                                <dd><a href="https://www.fiduswriter.org/2017/05/24/decentralized-creation-of-academic-documents-using-a-network-attached-storage-nas-server/#comment-900">https://www.fiduswriter.org/2017/05/24/decentralized-creation-of-academic-documents-using-a-network-attached-storage-nas-server/#comment-900</a>, [<a href="http://web.archive.org/web/20170530084434/https://www.fiduswriter.org/2017/05/24/decentralized-creation-of-academic-documents-using-a-network-attached-storage-nas-server/">archived</a>], <a href="http://csarven.ca/#i">Sarven Capadisli</a></dd>
+                                            </dl>
+                                        </li>
+
+                                        <li>11:45 — 12:30: Open demos, position statements, lightning talks, and other topics</li>
 
                                         <li>12:30 — ∞: Make it so!</li>
                                     </ul>
