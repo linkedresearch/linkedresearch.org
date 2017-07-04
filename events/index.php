c98b0212dc42e1422525ac6698d8bce8ff360f94
Sarven Capadisli <info@csarven.ca>
2017-07-04 19:58:39 +0200

    Restructure events in planning

--- events/index.php
+++ events/index.php
@@ -18,30 +18,23 @@
                         <div datatype="rdf:HTML" property="schema:description">
                             <ul>
                                 <li id="iswc-2017">
-                                    <h3>DeSemWeb (Workshop on Decentralizing the Semantic Web) at ISWC 2017 (end of October 2017)</h3>
-
-                                    <p><a href="http://iswc2017.desemweb.org/" rel="rdfs:seeAlso">DeSemWeb</a> takes a radical perspective by focusing solely on decentralized and client-side applications, to counterbalance the centralized discourse of other tracks.</p>
+                                    <p>DeSemWeb (Workshop on Decentralizing the Semantic Web) at ISWC 2017 (end of October 2017): <a href="http://iswc2017.desemweb.org/" rel="rdfs:seeAlso">DeSemWeb</a> takes a radical perspective by focusing solely on decentralized and client-side applications, to counterbalance the centralized discourse of other tracks.</p>
                                 </li>
 
                                 <li id="eswc-2017">
-                                    <h3>ESWC 2017 Workshop (end of May 2017)</h3>
-
-                                    <p>Three Linked Research advocates (<a href="http://csarven.ca/#i">Sarven Capadisli</a>, <a href="https://rhiaro.co.uk/#me">Amy Guy</a> and <a href="http://www.oerc.ox.ac.uk/people/dder">Dave de Roure</a>) are organising a workshop at <a href="http://2017.eswc-conferences.org/">ESWC 2017</a> entitled <em>Enabling Decentralised Scholarly Communication</em>. We invite all those who are interested to volunteer for our program committee.</p>
+                                    <p>ESWC 2017 Workshop (end of May 2017): Three Linked Research advocates (<a href="http://csarven.ca/#i">Sarven Capadisli</a>, <a href="https://rhiaro.co.uk/#me">Amy Guy</a> and <a href="http://www.oerc.ox.ac.uk/people/dder">Dave de Roure</a>) are organising a workshop at <a href="http://2017.eswc-conferences.org/">ESWC 2017</a> entitled <em>Enabling Decentralised Scholarly Communication</em>. We invite all those who are interested to volunteer for our program committee.</p>
 
                                     <p>We also invite those who are thinking about submitting to our <a href="/calls#call-for-enabling-linked-research">Call for Enabling Linked Research</a> to submit by the workshop deadline if you would like to present your work in person at ESWC 2017.</p>
 
                                     <p><a href="/events/eswc2017/" rel="rdfs:seeAlso">Read more</a>.</p>
                                 </li>
 
-                                <li id="www-2017">
-                                    <h3>WOW (Workshop on Web Observatories, Social Machines and Decentralisation) at WWW2017 (early April 2017)</h3>
+                                <li id="wow-2017">
+                                    <p>WOW (Workshop on Web Observatories, Social Machines and Decentralisation) at WWW2017 (early April 2017): <a href="http://sociam.org/wow2017/" rel="rdfs:seeAlso">WOW 2017</a> are integrating <em>decentralisation: development of architectures and interactions for digital scholarly publishing as a social machine</em> into their list of topics of interest, as well as encouraging dogfooding and accepting HTML/RDF submissions.</p>
+                                </li>
 
-                                    <p><a href="http://sociam.org/wow2017/" rel="rdfs:seeAlso">WOW 2017</a> are integrating <em>decentralisation: development of architectures and interactions for digital scholarly publishing as a social machine</em> into their list of topics of interest, as well as encouraging dogfooding and accepting HTML/RDF submissions.</p>
-
-
-                                    <h3>LDOW (Linked Data on the Web) at WWW2017 (early April 2017)</h3>
-
-                                    <p><a href="http://events.linkeddata.org/ldow2017/" rel="rdfs:seeAlso">LDOW 2016</a> raises the challenge for <q>Pioneering the Linked Open Research Cloud</q>, and promotes decentralisation and data ownership, and encourages participants to submit their contribution by publishing a document at a domain they control or consider sufficiently authoritative.</p>
+                                <li id="ldow-2017">
+                                    <p>LDOW (Linked Data on the Web) at WWW2017 (early April 2017): <a href="http://events.linkeddata.org/ldow2017/" rel="rdfs:seeAlso">LDOW 2016</a> raises the challenge for <q>Pioneering the Linked Open Research Cloud</q>, and promotes decentralisation and data ownership, and encourages participants to submit their contribution by publishing a document at a domain they control or consider sufficiently authoritative.</p>
                                 </li>
                             </ul>
                         </div>
