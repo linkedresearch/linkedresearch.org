85efdfc68db4470777fdaae322ba06e292c31331
Sarven Capadisli <info@csarven.ca>
2017-09-09 18:14:30 +0200

    Add title to pills

--- cloud.php
+++ cloud.php
@@ -31,7 +31,7 @@
             </div>
           </section>
 
-          <hr class="unicode-red-pill unicode-blue-pill" />
+          <hr class="unicode-red-pill unicode-blue-pill" title="“This is your last chance. After this, there is no turning back. You take the blue pill — the story ends, you wake up in your bed and believe whatever you want to believe. You take the red pill — you stay in Wonderland and I show you how deep the rabbit-hole goes.” — Morpheus, The Matrix (1999)." />
 
           <section id="keywords">
             <h2>Keywords</h2>
