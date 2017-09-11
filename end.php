827b1ad42b1c26ef7c679b91b9ff570ae072d44e
Sarven Capadisli <info@csarven.ca>
2017-09-11 14:42:28 +0200

    Remove document-license from footer

--- end.php
+++ end.php
@@ -1,5 +1,5 @@
     <footer>
-      <dl id="document-license">
+      <dl>
         <dt>License</dt>
         <dd><a rel="schema:license" href="https://creativecommons.org/publicdomain/zero/1.0/" title="CC0 1.0 Universal Public Domain Dedication">CC0 1.0 Universal</a></dd>
       </dl>
