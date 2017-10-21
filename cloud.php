213142a78b935b57577b0e545ed2821c0e1678e8
Sarven Capadisli <info@csarven.ca>
2017-10-21 09:08:16 -0600

    Keep the actor for the annotation notification open

--- cloud.php
+++ cloud.php
@@ -211,7 +211,7 @@
                       </figure>
 
                       <ul>
-                        <li><code>as:actor</code> is the URI of the agent that sent the notification. Normally a person, as opposed to software.</li>
+                        <li><code>as:actor</code> is the URI of the agent that sent the notification.</li>
                         <li>URI of an open/free culture license on the annotation is expected.</li>
                       </ul>
 
