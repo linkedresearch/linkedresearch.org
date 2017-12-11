592024e3dadaaa7d6b4f673aceee5f64fa4d24cc
Sarven Capadisli <info@csarven.ca>
2017-12-11 22:26:35 +0100

    Add article about the RCSC workshop

---
+++ events/the-web-conf-2018/index.php
@@ -0,0 +1,121 @@
+<? $title = "Researcher Centric Scholarly Communication at The Web Conf 2018"; ?>
+<? include '../../top.php'; ?>
+    <main>
+      <article about="" typeof="schema:Article">
+        <h1 property="schema:name"><?=$title;?></h1>
+
+        <dl id="document-inbox">
+          <dt>Notifications Inbox</dt>
+          <dd><a href="https://linkedresearch.org/inbox/linkedresearch.org/events/the-web-conf-2018/" rel="ldp:inbox">https://linkedresearch.org/inbox/linkedresearch.org/events/the-web-conf-2018/</a></dd>
+        </dl>
+
+        <dl id="document-in-reply-to">
+          <dt>In Reply To</dt>
+          <dd><a href="https://linkedresearch.org/calls" rel="as:inReplyTo">Call for Linked Research</a></dd>
+        </dl>
+
+        <dl id="document-see-also">
+          <dt>See Also</dt>
+          <dd><a href="https://linkedresearch.org/events/the-web-conf-2018/workshop-proposal" rel="rdfs:seeAlso">Workshop proposal for Researcher Centric Scholarly Communication to The Web Conf</a></dd>
+        </dl>
+
+        <dl id="document-event" rel="bibo:presentedAt" resource="#event">
+          <dt resource="#event" typeof="schema:Event">Event</dt>
+          <dd property="schema:description"><span property="schema:name">Researcher Centric Scholarly Communication</span> co-located with <a rel="schema:isPartOf" href="https://www2018.thewebconf.org/">The Web Conf 2018</a></dd>
+          <dt>Location</dt>
+          <dd rel="schema:location" resource="http://dbpedia.org/resource/Lyon">Lyon, France</dd>
+          <dt>Date</dt>
+          <dd><time content="2018-04-24" datatype="xsd:date" datetime="2018-04-24" property="dcterms:date">2018-04-24</time></dd>
+        </dl>
+
+        <div datatype="rdf:HTML" id="content" property="schema:description">
+          <section id="researcher-centric-scholarly-communication" rel="schema:hasPart" resource="#researcher-centric-scholarly-communication">
+            <h2 property="schema:name">Researcher Centric Scholarly Communication</h2>
+            <div datatype="rdf:HTML" property="schema:description">
+              <p>The purpose of this workshop is the mobilisation of a core group of researchers and practitioners to investigate the core characteristics of a minimal viable platform for scholarly communication that is researcher-centric, interlinked, and web-native. As visible outcomes, this workshop aims to deliver initial insights regarding these core characteristics and will determine the most appropriate approach to engage in a sustained effort aimed at tackling technical challenges and promoting adoption of solutions.</p>
+
+              <p>The workshop will be held at the <cite><a href="https://www2018.thewebconf.org/">The Web Conf 2018</a></cite> and host invited talks and research presentations. If you would like to share your research at the workshop, respond to the <a href="https://linkedresearch.org/calls">call</a>, and we will ensure your contribution is reviewed accordingly. We encourage all research contributions (articles and reviews) to be part of the <cite><a href="https://linkedresearch.org/cloud">Linked Open Research Cloud</a></cite> (LORC). Check it out!</p>
+
+              <section id="how" rel="schema:hasPart" resource="#how">
+                <h3>How?</h3>
+                <div datatype="rdf:HTML" property="schema:description">
+                  <p>Publish an article on the Web, and send us the URL. You are welcome to share it via <a href="https://gitter.im/linkedresearch/chat">community chat</a>. On <time>2018-02-25</time> we will snapshot the articles with <a href="https://archive.org/">archive.org</a>, and ensure they get at least two reviews each from an <a href="https://linkedresearch.org/calls#reviewers">open committe</a>. Of course, because your article is public, anyone else can give you feedback too. We will let you know if you are invited to present in person by <time>2018-03-11</time>.</p>
+                </div>
+              </section>
+
+              <section id="what" rel="schema:hasPart" resource="#what">
+                <h3>What?</h3>
+                <div datatype="rdf:HTML" property="schema:description">
+                  <p>The overarching topic for the workshop is web-based scholarly communication and its focus is on discussing how web standards and technologies can be used to achieve a <strong>minimal viable platform</strong> to establish a <a href="https://www.force11.org/scholarly-commons/principles" rel="cito:citesForInformation">scholarly commons</a> that meets the core requirements of a scholarly communication system, i.e. <a href="http://www.physik.uni-oldenburg.de/conferences/crisp97/roosendaal.html" rel="cito:citesForInformation">registration, awareness, certification, and archiving</a>, and that is accessible to both humans and machines.</p>
+                </div>
+              </section>
+
+              <section id="format" rel="schema:hasPart" resource="#format">
+                <h3>Format</h3>
+                <div datatype="rdf:HTML" property="schema:description">
+                  <p>The format of your contribution is up to you to decide how best to convey your message. Use the presentation timing (15 minutes) as a guideline for how much content you want to submit.</p>
+
+                  <p>The workshop will include an open dicussion session. We invite short blog post style responses to <em>“I can’t use the Web to publish my research because...”</em> which we will use to seed the open session.</p>
+                </div>
+              </section>
+
+              <section id="program" rel="schema:hasPart" resource="#program">
+                <h3>Program</h3>
+                <div datatype="rdf:HTML" property="schema:description">
+                  <p>Draft timeline..</p>
+
+                  <ul>
+                    <li>09:00 — 09:15: Hello world!</li>
+
+                    <li>09:15 — 09:30: Invited Talk</li>
+
+                    <li>09:30 — 09:45: Invited Talk</li>
+
+                    <li>09:45 — 10:00: Invited Talk</li>
+
+                    <li>10:00 — 10:30: Invited Talk</li>
+
+                    <li>10:30 — 11:00: Take a break</li>
+
+                    <li>10:15 — 11:30: Invited Talk</li>
+
+                    <li>11:30 — 11:45: <cite>Article</cite> [archived], Authors
+                      <dl>
+                        <dt>Reviews</dt>
+                        <dd>Review [archived], Author</dd>
+                      </dl>
+                    </li>
+
+                    <li>12:00 — 12:15: <cite>Article</cite> [archived], Authors
+                      <dl>
+                        <dt>Reviews</dt>
+                        <dd>Review [archived], Author</dd>
+                      </dl>
+                    </li>
+
+                    <li>12:15 — 12:30: <cite>Article</cite> [archived], Authors
+                      <dl>
+                        <dt>Reviews</dt>
+                        <dd>Review [archived], Author</dd>
+                      </dl>
+                    </li>
+
+                    <li>12:30 — ∞: Make it so!</li>
+                  </ul>
+                </div>
+              </section>
+
+              <section id="who" rel="schema:hasPart" resource="#who">
+                <h2>Who?</h2>
+                <div datatype="rdf:HTML" property="schema:description">
+                  <p>The workshop organisers are <a href="http://csarven.ca/#i">Sarven Capadisli</a> (University of Bonn), <a href="https://hvdsomp.info/profile#me">Herbert Van de Sompel</a> (Los Alamos National Laboratory) and <a href="http://users.ecs.soton.ac.uk/wh/">Wendy Hall</a> (University of Southampton).</p>
+
+                  <p>Research contributions will be reviewed by these great <a href="https://linkedresearch.org/calls#reviewers">individuals</a>, and their contributions will be publicly accessible.</p>
+                </div>
+              </section>
+            </div>
+          </section>
+        </div>
+      </article>
+    </main>
+<? include '../../end.php'; ?>
