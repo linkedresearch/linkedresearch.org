d8c23ca868361b713e92969ef33bb922fcce70d8
Sarven Capadisli <info@csarven.ca>
2019-07-16 21:28:52 +0200

    PHP to HTML

--- end.php
+++
@@ -1,8 +0,0 @@
-    <footer>
-      <dl>
-        <dt>License</dt>
-        <dd><a rel="schema:license" href="https://creativecommons.org/publicdomain/zero/1.0/" title="CC0 1.0 Universal Public Domain Dedication">CC0 1.0 Universal</a></dd>
-      </dl>
-    </footer>
-  </body>
-</html>
